1f47149396e73dd00e596b5698f79fd960112965
Stefan Popp <mail@stefanpopp.de>
2015-11-27 17:23:53 +0100

    Keyboard problem

--- KeyboardTest/ViewController.swift
+++ KeyboardTest/ViewController.swift
@@ -8,10 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol AutoKeyboardScrollable {
+    
+    func willShowKeyboard(notification: NSNotification)
+    func willHideKeyboard(notification: NSNotification)
+    func registerNotification()
+    func getContentScrollView() -> UIScrollView!
+    
+}
 
+extension AutoKeyboardScrollable where Self: UIViewController {
+    func registerNotification() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "willShowKeyboard:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "willHideKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func willShowKeyboard(notification: NSNotification) {
+        guard notification.userInfo != nil else { return }
+        guard let scrollView = getContentScrollView() else { return }
+        
+        let userInfo = notification.userInfo
+        let keyboardSize: CGSize = (userInfo![UIKeyboardFrameEndUserInfoKey]?.CGRectValue.size)!
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0)
+        
+        scrollView.scrollEnabled = true
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect: CGRect = self.view.frame
+        aRect.size.height -= keyboardSize.height
+        if !CGRectContainsPoint(aRect, self.view.frame.origin) {
+            scrollView.scrollRectToVisible(self.view.frame, animated: true)
+        }
+    }
+    
+    func willHideKeyboard(notification: NSNotification) {
+        guard let scrollView = getContentScrollView() else { return }
+        
+        let contentInset = UIEdgeInsetsZero;
+        
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = contentInset
+        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x, scrollView.contentOffset.y)
+        scrollView.scrollEnabled = false
+    }
+}
+
+class ViewController: UIViewController, AutoKeyboardScrollable {
+
+    @IBOutlet weak var contentScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerNotification()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +68,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Protocol stuff
+    func getContentScrollView() -> UIScrollView! {
+        return contentScrollView
+    }
+    
+//    func willShowKeyboard(notification: NSNotification) {
+//        guard notification.userInfo != nil else { return }
+//        guard let scrollView = getContentScrollView() else { return }
+//        
+//        let userInfo = notification.userInfo
+//        let keyboardSize: CGSize = (userInfo![UIKeyboardFrameEndUserInfoKey]?.CGRectValue.size)!
+//        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0)
+//        
+//        scrollView.scrollEnabled = true
+//        scrollView.contentInset = contentInsets
+//        scrollView.scrollIndicatorInsets = contentInsets
+//        
+//        var aRect: CGRect = self.view.frame
+//        aRect.size.height -= keyboardSize.height
+//        if !CGRectContainsPoint(aRect, self.view.frame.origin) {
+//            scrollView.scrollRectToVisible(self.view.frame, animated: true)
+//        }
+//    }
+//    
+//    func willHideKeyboard(notification: NSNotification) {
+//        guard let scrollView = getContentScrollView() else { return }
+//        
+//        let contentInset = UIEdgeInsetsZero;
+//        
+//        scrollView.contentInset = contentInset
+//        scrollView.scrollIndicatorInsets = contentInset
+//        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x, scrollView.contentOffset.y)
+//        scrollView.scrollEnabled = false
+//    }
 
 }
 
